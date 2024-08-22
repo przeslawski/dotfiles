db61d1ec086a023dedcddc38c1f5225936f8eddb
Jakub Przeslawski <kuba.przeslawski@gmail.com>
2024-08-23 01:13:54 +0200

    fix: use posix ln

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ln -s -t $HOME/.config $(realpath ./alacritty)
-ln -s -t $HOME/.config $(realpath ./nvim)
-ln -s -t $HOME $(realpath ./.tmux.conf)
+ln -s $(realpath ./alacritty) $HOME/.config
+ln -s $(realpath ./nvim) $HOME/.config
+ln -s $(realpath ./.tmux.conf) $HOME
