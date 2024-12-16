4ccb0dc21a2b0d00f8992fde8922fc20890b75d3
Jakub Przeslawski <kuba.przeslawski@gmail.com>
2024-12-16 11:34:54 +0100

    feat: start tmux iteroperability config

--- kickstart.nvim/init.lua
+++ kickstart.nvim/init.lua
@@ -946,6 +946,7 @@ require('lazy').setup({
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'custom.plugins.copilot-chat',
   require 'custom.plugins.copilot',
+  require 'custom.plugins.tmux',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
