c8c08019d52911790249c852c66efa46da72a497
Jakub Przesławski <prwj@viessmann.com>
2025-10-01 11:23:13 +0200

    fix(install): install starship if not found in the system

--- install.sh
+++ install.sh
@@ -9,6 +9,20 @@ ln -s $(realpath ./alacritty) $HOME/.config
 ln -s $(realpath ./nvim) $HOME/.config
 ln -s $(realpath ./.tmux.conf) $HOME
 
+set -x
+env
+
+if command -v starship &> /dev/null
+then
+    echo "Starship is already installed"
+else
+    echo "Installing starship..."
+    curl -sS https://starship.rs/install.sh > /tmp/install_starship.sh
+    chmod +x /tmp/install_starship.sh
+    mkdir -p $HOME/.local/bin
+    /tmp/install_starship.sh -y -b $HOME/.local/bin
+    rm /tmp/install_starship.sh
+fi
 export STARSHIP_CONFIG=./starship.toml
 
 if command -v zsh &> /dev/null
