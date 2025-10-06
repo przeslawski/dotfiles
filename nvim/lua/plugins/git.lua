caa92a2e9be4833b42679705004e516f09c45f91
Jakub Przesławski <prwj@viessmann.com>
2025-10-06 09:29:15 +0200

    refactor: rename kickstart.nvim to nvim so it can be used directly from dotfiles

--- nvim/lua/plugins/git.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "tpope/vim-fugitive",
-  cmd = {
-    "Git",
-    -- {
-    --   combine_args = true, -- false by default to keep existing behavior
-    -- },
-  },
-}
--- nvim/lua/plugins/uml.lua
+++
@@ -1,7 +0,0 @@
-return {
-  {
-    "aklt/plantuml-syntax",
-  },
-  --- todo: add keymaps for running plantuml generator in the background
-  --- keymaps only added if the filetype is plantuml
-}
